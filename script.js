2e73e5816506918943c8fc9ddfdd5402968e9791
unknown <polovnikovai19i@gmail.com>
2023-04-06 17:13:28 +0300

    мой первый коммит

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-let popup = document.querySelector(".popup");
-let popupContainer = document.querySelector(".popup__container");
-let openPopupButton = document.querySelectorAll('.profile__edit-button');
-let closePopupButton = document.querySelector(".popup__close-button");
+//Открытие и закрытие попапа
+let popup = document.querySelector('.popup');
+let popupContainer = document.querySelector('.popup__container');
+let openPopupButton = document.querySelector('.profile__edit-button');
+let closePopupButton = document.querySelector('.popup__close-button');
 
-openPopupButton.forEach((button) => { // Перебираем все кнопки
-  button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
-      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-      popup.classList.add('popup_opened'); // Добавляем класс 'active' для фона
-      popupContainer.classList.add('popup__container_opened'); // И для самого окна
-  })
-});
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
 
-closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
-  popup.classList.remove('popup_opened'); // Убираем активный класс с фона
-  popupContainer.classList.remove('popup__container_opened'); // И с окна
-});
+openPopupButton.addEventListener('click', openPopup);
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+closePopupButton.addEventListener('click', closePopup);
+
+//Редактирование имени и информации о себе
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__item_el_title');
+let jobInput = document.querySelector('.popup__item_el_subtitle');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+nameInput.value = profileTitle.textContent;
+jobInput.value = profileSubtitle.textContent;
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
 
