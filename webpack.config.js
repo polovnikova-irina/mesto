0963ffe048419e8418ba1fd43da98c68ae0b4da1
unknown <polovnikovai19i@gmail.com>
2023-06-13 11:58:32 +0300

    собрать вебпак

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: './src/pages/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -50,4 +50,4 @@ module.exports = {
     new MiniCssExtractPlugin(),
 
   ]
-} 
+}
