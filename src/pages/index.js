f8a968324a6880d335cfa4a74ceb546b0c896860
polovnikova-irina <polovnikovai19i@gmail.com>
2023-06-30 16:30:25 +0300

    9sprintfinally

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ import {
 import Card from "../scripts/components/Card.js";
 import FormValidator from "../scripts/components/FormValidator.js";
 import Section from "../scripts/components/Section.js";
-import Popup from "../scripts/components/Popup.js";
 import PopupWithImage from "../scripts/components/PopupWithImage.js";
 import PopupWithForm from "../scripts/components/PopupWithForm.js";
 import UserInfo from "../scripts/components/UserInfo.js";
